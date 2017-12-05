c2a2d0cd93f6b531f41d8f62582b8ebd685d9ccf
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2017-12-04 22:14:19 -0800

    implement setup.py (fixes #2)

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='pdsc',
+    version="1.0",
+    description='PDS Coincidences',
+    author='Gary Doran',
+    author_email='Gary.B.Doran.Jr@jpl.nasa.gov',
+    url='https://github-fn.jpl.nasa.gov/COSMIC/COSMIC_PDSC',
+    license="BSD compatable (see the LICENSE file)",
+    packages=['pdsc'],
+    platforms=['unix'],
+    scripts=[
+        'bin/pdsc_injest',
+        'bin/pdsc_server',
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'Polygon2',
+        'progressbar',
+        'geographiclib',
+        'CherryPy',
+    ],
+    provides=[
+        'pdsc',
+    ],
+    include_package_data=True
+)
