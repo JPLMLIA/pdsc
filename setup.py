dd50e1f78cd8ec90a7b40580df3a2a35de246c83
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2019-12-24 13:16:11 -0500

    update setup.py for Python 3 dependencies
    Issue: #3

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 import os
+import sys
 from setuptools import setup
 
 # brings in "version" and "description" vars
-execfile(os.path.join('pdsc', 'version.py'))
+versionfile = 'version.py'
+versionpath = os.path.join('pdsc', versionfile)
+with open(versionpath, 'r') as f:
+    code = compile(f.read(), versionfile, 'exec')
+    exec(code)
 
-setup(name='pdsc',
-    version=__version__,
-    description=__description__,
-    author='Gary Doran',
-    author_email='Gary.B.Doran.Jr@jpl.nasa.gov',
-    url='https://github-fn.jpl.nasa.gov/COSMIC/COSMIC_PDSC',
-    license="BSD compatable (see the LICENSE file)",
-    packages=['pdsc'],
-    platforms=['unix'],
-    scripts=[
-        'bin/pdsc_util',
-        'bin/pdsc_ingest',
-        'bin/pdsc_server',
-    ],
+if sys.version_info[0] == 2:
     install_requires=[
         'tempora<=1.13',
         'more-itertools<5.0.0',
@@ -31,7 +23,38 @@ setup(name='pdsc',
         'CherryPy<18.0.0',
         'requests',
         'future',
+    ]
+
+elif sys.version_info[0] == 3:
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'Polygon3',
+        'progressbar',
+        'PyYAML',
+        'geographiclib',
+        'CherryPy',
+        'requests',
+        'future',
+    ]
+
+setup(name='pdsc',
+    version=__version__,
+    description=__description__,
+    author='Gary Doran',
+    author_email='Gary.B.Doran.Jr@jpl.nasa.gov',
+    url='https://github-fn.jpl.nasa.gov/COSMIC/COSMIC_PDSC',
+    license="BSD compatable (see the LICENSE file)",
+    packages=['pdsc'],
+    platforms=['unix'],
+    python_requires='>=2.7',
+    scripts=[
+        'bin/pdsc_util',
+        'bin/pdsc_ingest',
+        'bin/pdsc_server',
     ],
+    install_requires=install_requires,
     provides=[
         'pdsc',
     ],
